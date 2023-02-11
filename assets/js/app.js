af16e2595ae355cdbd44cf2820b69d029d979daa
Musharrof Shishir <musharrofshishir@gmail.com>
2023-02-12 02:00:13 +0600

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Flick Carousel
+var flkty = new Flickity( '._login_slider', {
+  prevNextButtons: false,
+  pageDots: false,
+  autoPlay: true,
+  autoPlay: 3500,
+  wrapAround: true,
+  pauseAutoPlayOnHover: false,
+  fade: true,
+});
+// Flick Carousel
